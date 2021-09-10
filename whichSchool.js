c3da847665187924f154b71d1595abcf5c7f9dd2
Todd Demone <tdemone1@gmail.com>
2021-09-10 19:48:52 +0000

    my code for the activity 'Conditionals - whichSchool'

---
+++ whichSchool.js
@@ -0,0 +1,13 @@
+const age = 18;
+
+const whichSchool = function(age) {
+  if (age < 13) {
+    return "Elementary School";
+  } else if (age >= 13 && age <= 18) {
+    return "Secondary School";
+  } else {
+    return 'Lighthouse Labs';
+  }
+}
+
+console.log(whichSchool(age));
