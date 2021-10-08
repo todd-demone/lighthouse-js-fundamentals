5dce086687a2617fe029cc5ac8348379d460d2ec
Todd Demone <tdemone1@gmail.com>
2021-10-08 17:39:05 +0000

    completed kata 9

---
+++ camelCase.js
@@ -0,0 +1,26 @@
+// camelCase
+// String => String
+const camelCase = function(input) {
+  let str = '';
+  let capitalize = false;
+  // loop through string letter by letter
+  for (char of input) {
+    // a flag to tell whether to capitalize a letter
+    if (char == ' ') { 
+      capitalize = true;
+      continue;
+    } else {
+      if (capitalize) {
+        str += char.toUpperCase();
+        capitalize = false;
+      } else {
+        str += char;
+      }
+    }
+  }
+  return str;
+};
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
