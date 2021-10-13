e365e825c64fe525092c40607dd16640a65a3a10
Todd Demone <tdemone1@gmail.com>
2021-10-13 14:44:16 +0000

    completed Bouncy Castles kata

--- bouncyCastles.js
+++ bouncyCastles.js
@@ -13,9 +13,45 @@ const coneVolume = function(radius, height) {
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
 
 const prismVolume = function(height, width, depth) {
-
+  return (width * depth) * height; 
 };
+
+console.log(prismVolume(3, 4, 5) === 60);
 
 const totalVolume = function (solids) {
-
+  let totalVol = 0;
+  for (shape of solids) {
+    if (shape.type === 'sphere') {
+      totalVol += sphereVolume(shape.radius);
+    } else if (shape.type === 'cone') {
+      totalVol += coneVolume(shape.radius, shape.height);
+    } else if (shape.type === 'prism') {
+      totalVol += prismVolume(shape.height, shape.width, shape.depth);
+    }
+  }
+  return totalVol;
 };
+
+const largeSphere = {
+  type: 'sphere',
+  radius: 40
+}
+
+const smallSphere = {
+  type: 'sphere',
+  radius: 10
+}
+
+const cone = {
+  type: 'cone',
+  radius: 3,
+  height: 5
+}
+
+const duck = [
+  largeSphere,
+  smallSphere,
+  cone
+]
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
